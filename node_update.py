b2c19f1f59a7a24fec44a756f6942c39e66105f1
mskim8717 <ms.kim8717@gmail.com>
2019-05-25 09:18:45 +0900

    update for nodeno information

--- node_update.py
+++ node_update.py
@@ -31,38 +31,26 @@ service_key= '개인별 인증키 입력'
 # 버스노선정보조회서비스 - 노선별경유정류소목록조회
 url_getRouteAcctoThrghSttnList = 'http://openapi.tago.go.kr/openapi/service/BusRouteInfoInqireService/getRouteAcctoThrghSttnList'
 
+# 버스정류소정보조회서비스 - 정류소번호목록조회
+url_getSttnNoList = 'http://openapi.tago.go.kr/openapi/service/BusSttnInfoInqireService/getSttnNoList'
+
 # 버스노선정보조회서비스 - 노선번호목록조회
 url_getRouteNoList = 'http://openapi.tago.go.kr/openapi/service/BusRouteInfoInqireService/getRouteNoList'
 
-# 도착정보조회서비스
+# 도착정보조회서비스 - 정류소별도착예정정보목록조회
 url_getSttnAcctoArvlPrearngeInfoList = 'http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService/getSttnAcctoArvlPrearngeInfoList'
 
 
 def execute():
-    for routeno_list in routeNo:
-        print( routeno_list, '검색시작 ...' )
-        for routeno in routeno_list: # 900, 990, 1004, 1005, ...
-            print('[검색시작] 노선번호:', routeno)
-            queryParams = '?' + 'serviceKey=' + service_key + '&' + urlencode( {quote_plus( 'cityCode' ): '12',
-                                                                                quote_plus( 'routeNo' ): routeno},
-                                                                               encoding='UTF-8' )
-            request = Request( url_getRouteNoList + queryParams )
-            response = urlopen( request )
-            rescode = response.getcode()
-            if (rescode == 200):
-                response_body = response.read()
-                soup = BeautifulSoup( response_body, "lxml" )
-            else:
-                print( "Error Code:" + rescode )
-
-            for row, data in enumerate( soup.find_all( 'item' ) ): # 900:SJB293000182,SJB293000183, 990:SJB...., ...
-                routeid = data.find( 'routeid' ).text
-                print('[검색시작] 노선ID:', routeid)
+    try:
+        for routeno_list in routeNo:
+            print( routeno_list, '검색시작 ...' )
+            for routeno in routeno_list: # 900, 990, 1004, 1005, ...
+                print('[검색시작] 노선번호:', routeno)
                 queryParams = '?' + 'serviceKey=' + service_key + '&' + urlencode( {quote_plus( 'cityCode' ): '12',
-                                                                                    quote_plus( 'routeId' ): routeid,
-                                                                                    quote_plus( 'numOfRows' ): 80},
+                                                                                    quote_plus( 'routeNo' ): routeno},
                                                                                    encoding='UTF-8' )
-                request = Request( url_getRouteAcctoThrghSttnList + queryParams )
+                request = Request( url_getRouteNoList + queryParams )
                 response = urlopen( request )
                 rescode = response.getcode()
                 if (rescode == 200):
@@ -71,15 +59,14 @@ def execute():
                 else:
                     print( "Error Code:" + rescode )
 
-                for row, data in enumerate( soup.find_all( 'item' ) ):
-                    nodeno = data.find( 'nodeno' ).text
-                    nodeid = data.find( 'nodeid' ).text
-                    print('[검색시작] 정류소번호:',nodeno, '정류소ID:',nodeid)
-
+                for row, data in enumerate( soup.find_all( 'item' ) ): # 900:SJB293000182,SJB293000183, 990:SJB...., ...
+                    routeid = data.find( 'routeid' ).text
+                    print('[검색시작] 노선ID:', routeid)
                     queryParams = '?' + 'serviceKey=' + service_key + '&' + urlencode( {quote_plus( 'cityCode' ): '12',
-                                                                                        quote_plus( 'nodeId' ): nodeid},
+                                                                                        quote_plus( 'routeId' ): routeid,
+                                                                                        quote_plus( 'numOfRows' ): 80},
                                                                                        encoding='UTF-8' )
-                    request = Request( url_getSttnAcctoArvlPrearngeInfoList + queryParams )
+                    request = Request( url_getRouteAcctoThrghSttnList + queryParams )
                     response = urlopen( request )
                     rescode = response.getcode()
                     if (rescode == 200):
@@ -88,35 +75,81 @@ def execute():
                     else:
                         print( "Error Code:" + rescode )
 
-                    find_routeno = "fail"
-                    status = "fail"
-                    existStatus = None
-                    if soup.find_all( 'resultmsg' ) == 'NORMAL SERVICE.':
-                        for row, data in enumerate( soup.find_all( 'item' ) ):
-                            find_routeno = data.find( 'routeno' ).text
-                            conn = sqlite3.connect( "node_update.db" )
-                            with conn:
-                                cur = conn.cursor()
-                                cur.execute("select * from nodeInfo where routeid = ? AND nodeid = ? AND nodeno = ? AND routeno = ?", (routeid, nodeid, nodeno, find_routeno))
-                                existStatus = cur.fetchone()
-                                if existStatus is None:
-                                    cur.execute(
-                                        "INSERT INTO nodeInfo(routeid, nodeid, nodeno, routeno) SELECT ?, ?, ?, ? WHERE NOT EXISTS(SELECT(routeid, nodeid, nodeno, routeno) FROM nodeInfo WHERE routeid = ? AND nodeid = ? AND nodeno = ? AND routeno = ?)",
-                                        (routeid, nodeid, nodeno, find_routeno, routeid, nodeid, nodeno, find_routeno) )
-                                    conn.commit()
-                                    status = "new"
+                    for row, data in enumerate( soup.find_all( 'item' ) ):
+                        nodeno = data.find( 'nodeno' ).text
+                        nodeid = data.find( 'nodeid' ).text
+
+                        queryParams = '?' + 'serviceKey=' + service_key + '&' + urlencode(
+                            {quote_plus('cityCode'): '12',
+                             quote_plus('nodeNo'): nodeno},
+                            encoding='UTF-8')
+                        request = Request(url_getSttnNoList + queryParams)
+                        response = urlopen(request)
+                        rescode = response.getcode()
+                        if (rescode == 200):
+                            response_body = response.read()
+                            soup = BeautifulSoup(response_body, "lxml")
+                        else:
+                            print("Error Code:" + rescode)
+
+                        nodenm = soup.find('nodenm').text
+                        gpslati = soup.find('gpslati').text
+                        gpslong = soup.find('gpslong').text
+
+                        conn = sqlite3.connect( server_path + "node_update.db" )
+                        with conn:
+                            cur = conn.cursor()
+                            cur.execute("select * from nodeInfo where routeno = ? AND routeid = ? AND nodeno = ? AND nodeid = ? AND nodenm = ? AND gpslati = ? AND gpslong = ?",(routeno, routeid, nodeno, nodeid, nodenm, gpslati, gpslong) )
+                            dbExistStatus = cur.fetchone() # 데이터베이스에 이미 업데이트 된 상태인지 확인
+                        conn.close()
+
+                        print('[검색시작] 노선번호:', routeno, '정류소번호:',nodeno, '정류소ID:',nodeid, 'DB검색결과:', dbExistStatus)
+
+                        if dbExistStatus is None:
+                            print('[DB응답] 신규 데이터베이스 업데이트.')
+                            queryParams = '?' + 'serviceKey=' + service_key + '&' + urlencode(
+                                {quote_plus( 'cityCode' ): '12',
+                                 quote_plus( 'nodeId' ): nodeid},
+                                encoding='UTF-8' )
+                            request = Request( url_getSttnAcctoArvlPrearngeInfoList + queryParams )
+                            response = urlopen( request )
+                            rescode = response.getcode()
+                            if (rescode == 200):
+                                response_body = response.read()
+                                soup = BeautifulSoup( response_body, "lxml" )
+                            else:
+                                print( "Error Code:" + rescode )
+
+                            find_routeno = "검색결과 없음"
+                            print('resultmsg:', soup.find( 'resultmsg' ).text)
+                            if soup.find( 'resultmsg' ).text == 'NORMAL SERVICE.':
+                                for row, data in enumerate( soup.find_all( 'item' ) ):
+                                    find_routeno = data.find( 'routeno' ).text # '도착정보조회서비스'를 통해 검색 된 해당 정류장을 지나가는 노선번호
+                                    conn = sqlite3.connect( "node_update.db" )
+                                    with conn:
+                                        cur = conn.cursor()
+                                        cur.execute(
+                                                "INSERT INTO nodeInfo(updateTime, routeno, routeid, nodeno, nodeid, nodenm, gpslati, gpslong, find_routeno) SELECT ?, ?, ?, ?, ?, ?, ?, ?, ? WHERE NOT EXISTS(SELECT(updateTime, routeno, routeid, nodeno, nodeid, nodenm, gpslati, gpslong, find_routeno) FROM nodeInfo WHERE routeno = ? AND routeid = ? AND nodeno = ? AND nodeid = ? AND nodenm = ? AND gpslati = ? AND gpslong = ? AND find_routeno = ?)",
+                                                (str(datetime.now()), routeno, routeid, nodeno, nodeid, nodenm, gpslati, gpslong, find_routeno, routeno, routeid, nodeno, nodeid, nodenm, gpslati, gpslong, find_routeno) )
+                                        conn.commit()
+                                        print( '[검색시작] 찾은노선번호:', find_routeno, data.find( 'arrtime' ).text )
+                                    print( '[검색종료]' )
+                                    conn.close()
+                                print( '업데이트 시간:', datetime.now(), '노선번호:', routeno, '노선ID:', routeid, '정류소번호:', nodeno,
+                                       '정류소ID:', nodeid, '정류소명:', nodenm, '위도:',gpslati, '경도:',gpslong, '검색버스정보:', find_routeno, '\n' )
+                                if find_routeno != 'fail':
+                                    time.sleep( 10 )
                                 else:
-                                    status = 'already'
-                                print( '[검색시작] 찾은노선번호:', find_routeno, data.find( 'arrtime' ).text, existStatus, status )
-                            print( '[검색종료]' )
-                            conn.close()
-                        print( datetime.now(), routeno, routeid, nodeid, nodeno, 'find:',find_routeno, 'status:', status, existStatus, '\n' )
-                        time.sleep(10)
-                    else:
-                        print( '[검색 제한으로 인한 프로그램 종료]' )
-                        sys.exit()
-    print( '[프로그램 종료]' )
-    sys.exit()
+                                    time.sleep(3)
+                            else:
+                                print( '[검색 제한으로 인한 프로그램 종료]' )
+                                sys.exit()
+                        else:
+                            print('[DB응답] 이미 데이터베이스에 등록이 되어 있습니다.')
+        print( '[프로그램 종료]' )
+        sys.exit()
+    except:
+        print('exception')
 
 while True:
     execute()
